d2308644e8c5a40834a04b98b553446dba035a9a
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2024-12-21 19:26:12 +0200

    Initial Commit

---
+++ SwiftUiMVVM/SwiftUiMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMVVMApp.swift
+//  SwiftUiMVVM
+//
+//  Created by Kirill Khomicevich on 21.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
